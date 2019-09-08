68cb7be0634be3eef6aa796b2829c6d0f6d589b4
Damir Arh <damir.arh@gmail.com>
2019-09-08 13:44:18 +0200

    Wrap functionality into a console app

--- PirateCruise/Program.cs
+++ PirateCruise/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace PirateCruise
 {
@@ -6,7 +10,32 @@ namespace PirateCruise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        [Required]
+        [Option(Description = "GPX file with cache details")]
+        public string Input { get; }
+
+        [Required]
+        [Option(Description = "CSV file with calculated coordinates")]
+        public string Output { get; }
+
+        private void OnExecute()
+        {
+            var gpxDocument = XDocument.Load(Input);
+
+            var gpxParser = new GpxFileParser();
+            var parsedCaches = gpxParser.Parse(gpxDocument);
+
+            var processor = new CacheProcessor();
+            var processedCaches = parsedCaches.Select(cache => processor.Process(cache));
+
+            var csvWriter = new CsvFileWriter();
+            using(var writer = new StreamWriter(Output))
+            {
+                csvWriter.Write(processedCaches, writer);
+            }
         }
     }
 }
